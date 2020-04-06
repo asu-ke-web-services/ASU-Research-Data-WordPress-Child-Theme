e76a8beebaf5e58b57764bbd726df1a515100953
Nathan Rollins <nathan.rollins@asu.edu>
2020-04-06 14:47:11 -0700

    Disable Pages from WP search results
    Only return Posts in results

--- functions.php
+++ functions.php
@@ -6,3 +6,12 @@
  *
  * @package asu-research-data-wordpress-child
  */
+
+ // Remove Pages from search results (only return Post results)
+function exclude_pages_from_search($query) {
+    if ( $query->is_main_query() && is_search() ) {
+        $query->set( 'post_type', 'post' );
+    }
+    return $query;
+}
+add_filter( 'pre_get_posts','exclude_pages_from_search' );
